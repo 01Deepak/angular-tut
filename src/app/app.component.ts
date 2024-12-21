0eb75a27e1117e1cbe7327db86b6256bc6d89764
Deepak Singh <deepak7275759723@gmail.com>
2024-12-21 22:47:10 +0530

    property binding in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from "./component/counter/counter.component";
+import { PropertyBindingComponent } from "./component/property-binding/property-binding.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CounterComponent],
+  imports: [RouterOutlet, CounterComponent, PropertyBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
