a037c7593fb71aab97422416d661c655e6dccbb4
Deepak Singh <deepak7275759723@gmail.com>
2025-01-09 10:44:38 +0530

    validation in template driven form

--- src/app/component/forms/template-driven-form/template-driven-form.component.ts
+++ src/app/component/forms/template-driven-form/template-driven-form.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-template-driven-form',
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf, CommonModule],
   templateUrl: './template-driven-form.component.html',
   styleUrl: './template-driven-form.component.scss'
 })
 export class TemplateDrivenFormComponent {
-  handleLoginSubmit(data: any) {
-    console.log(data);
+  handleLoginSubmit(form: any) {
+    if (form.valid) {
+      console.log("form is valid = ", form.value);
+    }else {
+      console.log("form is invalid = ", form.value);
+    }
+  }
+
+  checkLogs(data: any) {
+    console.log("logs = ",data.errors);
   }
 }
