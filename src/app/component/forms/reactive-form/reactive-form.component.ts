13c37124e91311199be59c6cc52db1ddbe9fbf71
Deepak Singh <deepak7275759723@gmail.com>
2025-01-09 19:15:49 +0530

    reactive form validation

--- src/app/component/forms/reactive-form/reactive-form.component.ts
+++ src/app/component/forms/reactive-form/reactive-form.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl,ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl,Validators,ReactiveFormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-reactive-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,NgIf],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.scss'
 })
 export class ReactiveFormComponent {
   loginForm = new FormGroup({
-    name: new FormControl(),
-    password: new FormControl(),
+    name: new FormControl("",[Validators.required]),
+    password: new FormControl("",Validators.required),
   })
+
+  get name(){
+    return this.loginForm.get('name');
+  }
+  get password(){
+    return this.loginForm.get('password');
+  }
+
+  checkLogs(data: any) {
+    console.log("logs = ",data);
+  }
 
   handleSubmit(form:any) {
     console.log(form.value);
